4b74661a8da7968dd33e0713aee50e817d6bafb4
SJ_ZHOU <32764628+yunzhuz@users.noreply.github.com>
2019-08-27 11:34:28 +0800

    Update 38-2----.py

--- 38-2----.py
+++ 38-2----.py
@@ -1,10 +1,19 @@
 def solution(s):
     l = list(s)
     result = []
-    for i in l:
-        result.append(i)
-    zuhe(s,result)
+    for i in range(1,len(l)+1):
+        zuhe(l,result,i)
 
-def zuhe(s,result):
-    for i range(len(s)):
+def zuhe(l,result,m):
+    if len(result) == m:
+        print(''.join(result))
+        return
+    if l:
+        result.append(l[0])
+        zuhe(l[1:],result,m)
+        result.pop()
+        zuhe(l[1:],result,m)
+        
+s = 'abc'
+solution(s)
         
